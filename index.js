9fffab8df7309217dee6869c20d122d1b5499849
unknown <berrymatondo@gmail.com>
2022-08-24 09:37:14 +0200

    ex

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ io.on("connection", (socket) => {
   console.log("User connected", socket.id);
 
   socket.on("send_message", (data) => {
+    console.log(data);
     socket.broadcast.emit("receive_message", data);
   });
 });
